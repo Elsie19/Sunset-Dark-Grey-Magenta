de75f2db4fc71e7fb50e396a4838398839cfce13
Henryws <58742515+Henryws@users.noreply.github.com>
2020-05-07 13:51:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,17 @@ cd /tmp
 
 git clone https://github.com/Henryws/Sunset-Dark-Grey-Magenta.git
 
-sudo tar xvzf oomox-Sunset-Dark-Grey-Magenta.tar.gz
-
 PACKAGE="/usr/share/themes/oomox-Sunset-Dark-Grey-Magenta/"
 
 if [[ -d $PACKAGE ]]; then
     echo "Already installed, to re-install remove the $PACKAGE dir"
     exit 1
 else
-    echo "Installation in $PACKAGE ..."
-
-    sudo mv oomox-Sunset-Dark-Grey-Magenta $PACKAGE
+    echo "Installing to $PACKAGE ..."
+    mkdir /usr/share/themes/oomox-Sunset-Dark-Grey-Magenta
+    sudo tar xvzf oomox-Sunset-Dark-Grey-Magenta $PACKAGE
 
     echo Done
-    cd /usr/share/themes
-    rm -rf Sunet-Dark-Grey-Magenta
-
 fi
 
 exit 0
